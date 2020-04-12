26be86ffd913ab19c389629cabda8be396756d0f
Arthiran Sivarajah <arthiran04@gmail.com>
2020-04-12 19:01:29 -0400

    Push before testing new joints

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Hips;
+    [SerializeField]
+    private Vector3 offset;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.position = Hips.position + offset;
+    }
+}
--- Assets/Scripts/FootGrounded.cs
+++ Assets/Scripts/FootGrounded.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class FootGrounded : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] Feet;
+    public Transform[] FootTarget;
 
-    // Update is called once per frame
-    void Update()
+    public LayerMask myLayerMask;
+
+    private bool Foot1Grounded = true;
+    private bool Foot2Grounded = true;
+    private int groundedCounter = 0;
+
+    [SerializeField]
+    private Rigidbody[] rb;
+    [SerializeField]
+    private float uprightForce;
+
+    public void LateUpdate()
     {
-        
+        //footsteps
+        for (int i = 0; i < FootTarget.Length; i++)
+        {
+            var foot = FootTarget[i];
+            var ray = new Ray(foot.transform.position + Vector3.up * 0.5f, Vector3.down);
+            var hitInfo = new RaycastHit();
+            if (Physics.Raycast(foot.position, -transform.up, out hitInfo, 0.05f, myLayerMask))
+            {
+                foot.position = hitInfo.point + Vector3.up * 0.05f;
+                //foot.localRotation = Quaternion.FromToRotation(transform.up, hitInfo.normal);
+            }
+            if (Physics.SphereCast(ray, 0.05f, out hitInfo, 0.5f, myLayerMask))
+            {
+                if (i == 0)
+                {
+                    Foot1Grounded = true;
+                }
+                else if (i == 1)
+                {
+                    Foot2Grounded = true;
+                }
+            }
+            else
+            {
+                if (i == 0)
+                {
+                    Foot1Grounded = false;
+                }
+                else if (i == 1)
+                {
+                    Foot2Grounded = false;
+                }
+            }
+        }
+        if (!Foot1Grounded || !Foot2Grounded)
+        {
+            for (int i = 0; i < rb.Length; i++)
+            {
+                rb[i].AddForce(transform.up * uprightForce / 1.1f * Time.deltaTime);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < rb.Length; i++)
+            {
+                rb[i].AddForce(transform.up * uprightForce * Time.deltaTime);
+            }
+        }
+
+
+        if (!Foot1Grounded && !Foot2Grounded)
+        {
+            for (int i = 0; i < FootTarget.Length; i++)
+            {
+                FootTarget[i].GetComponent<Rigidbody>().isKinematic = false;
+            }
+
+        }
+        else
+        {
+            for (int i = 0; i < FootTarget.Length; i++)
+            {
+                FootTarget[i].GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
     }
 }
